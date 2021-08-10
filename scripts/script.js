12a895be36e8949b4e47ecdf1f7720014a7084e3
Cabidge <cabbageboi69@gmail.com>
2021-08-10 10:47:39 -0400

    Set up 4x4 grid and mouseover event

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const gridContainer = document.querySelector("#grid-container");
+
+for (let i = 0; i < 16; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    gridContainer.appendChild(cell);
+    cell.addEventListener("mouseover", console.log);
+}
